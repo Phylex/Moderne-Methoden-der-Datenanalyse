0c24c1baff2808a99947f13b76cabd5bfbe1f671
Alexande Becker <nabla.becker@mailbox.org>
2020-05-30 22:41:51 +0200

    fixed the rejection method to create random numbers in the required intervals to create the rnums properly

--- statmeth.py
+++ statmeth.py
@@ -8,16 +8,21 @@
 """
 import numpy as np
 
-def generate_pdf_rnums_rejection_method(num, pdf, *args):
+def generate_pdf_rnums_rejection_method(num, pdf, bounds, *args):
     """generate random numbers following the pdf given for
-    pdf(*args) using the rejection method"""
+    pdf(*args) using the rejection method and transform the area according tho the pdf"""
     count = 0
+    rgen = 0
     transformed_rnums = []
+    delta = bounds[1]-bounds[0]
+    maxval = max(pdf(np.linspace(bounds[0], bounds[1], int(delta*1000), endpoint=True), *args))
     while count < num:
-        coordinate = np.random.rand(2)
+        coordinate = np.random.rand(2) * np.array([delta, maxval+.1]) + bounds[0]
+        rgen += 2
         if coordinate[1] < pdf(coordinate[0], *args):
             transformed_rnums.append(coordinate[0])
             count += 1
+    print(rgen)
     return transformed_rnums
 
 
