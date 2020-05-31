b759e8667147d0dbe1b534ca3a0a0c6c735f6c26
Alexande Becker <nabla.becker@mailbox.org>
2020-05-31 12:06:30 +0200

    slightly adapted the generate_pdf_rmuns_rejection_method to return the amount of rnums generated to come to the desired number. Also added a method for trapezoidal integration and one for statistical integration

--- statmeth.py
+++ statmeth.py
@@ -18,15 +18,28 @@ def generate_pdf_rnums_rejection_method(num, pdf, bounds, *args):
     maxval = max(pdf(np.linspace(bounds[0], bounds[1], int(delta*1000), endpoint=True), *args))
     while count < num:
         coordinate = np.random.rand(2) * np.array([delta, maxval+.1]) + bounds[0]
-        rgen += 2
+        rgen += 1
         if coordinate[1] < pdf(coordinate[0], *args):
             transformed_rnums.append(coordinate[0])
             count += 1
-    print(rgen)
-    return transformed_rnums
+    return transformed_rnums, rgen
 
 
 def transform_to_exp_decay(urnums, theta):
     """transforms uniformally distributed random numbers to random numbers
     with a distribution following an exponential decay"""
     return -theta * np.log(theta*urnums)
+
+def numerical_integrate_1D(func, bounds, *params):
+    """numerically integrate the function f(x, *params) inside the
+    interval given by bounds using the trapezoidal method"""
+    delta_b = np.abs(bounds[1]-bounds[0])
+    domain = np.linspace(bounds[0], bounds[1], int(delta_b*1000))
+    delta_x = domain[1:] - domain[:-1]
+    return sum((func(domain[:-1], *params) + func(domain[1:], *params))/2 + delta_x)
+
+def statistical_integrate_1D(func, bounds, *params):
+    """use the statistical method to integrate f"""
+    delta_b = bounds[1]-bounds[0]
+    data, rlen = generate_pdf_rnums_rejection_method(int(delta_b*1000), func, bounds, *params)
+    return len(data)/rlen
