337d12585d7251ad58992c116ee60971ede2c69b
Alexande Becker <becker.alexander42@gmail.com>
2020-06-01 14:03:32 +0200

    added multi color support to the likelihood scan and added various secondary traits to the parameter variation such as title etc.

--- statplot.py
+++ statplot.py
@@ -9,10 +9,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import statmeth as stm
 
-def plot_likelihood_scan_1D(scan_vals, scan_domain, parameter_name, plot_delta=False):
+PALETTS = {'blue-green': ['navy', 'blue', 'royalblue', 'dodgerblue',
+                          'deepskyblue', 'darkturquoise', 'turquoise', 'aqua', 'aquamarine'],
+           'green-yellow': ['darkgreen', 'green', 'limegreen', 'lime', 'lawngreen',
+                            'greenyellow', 'yellowgreen', 'olivedrap', 'darkolivegreen'],
+           'red-yellow': ['firebrick', 'lightcoral', 'coral',
+                          'orangered', 'red', 'darkorange', 'orange', 'gold', 'goldenrod'],
+          }
+
+def plot_likelihood_scan_1D(scan_vals, scan_domain, parameter_name, title='Likelihood scan', palette='blue-green', plot_delta=False):
     #convert the scan vals to an np.array
     scan_vals = np.array(scan_vals)
-    theta_min, theta_max = min(scan_domain), max(scan_domain)
     min_nll = min(scan_vals)
     min_nll_idx = scan_vals.argmin()
     if plot_delta:
@@ -22,37 +29,56 @@ def plot_likelihood_scan_1D(scan_vals, scan_domain, parameter_name, plot_delta=F
     #so that it is possible to find the closest array vals to +/- sigma
     left_side = scan_vals[:min_nll_idx]
     right_side = scan_vals[min_nll_idx:]
-    left_sigma, ls_idx = stm.find_nearest(left_side, min_nll+.5)
-    right_sigma, rs_idx = stm.find_nearest(right_side, min_nll+.5)
+    _left_sigma, ls_idx = stm.find_nearest(left_side, min_nll+.5)
+    _right_sigma, rs_idx = stm.find_nearest(right_side, min_nll+.5)
     rs_idx = rs_idx + len(left_side)
 
     if plot_delta:
         scan_vals = scan_vals - min_nll
 
     #plot likelihood scan
-    plt.plot(scan_domain, scan_vals, color='darkblue', marker='o',
+    #set up color iterator
+    color_it = iter(PALETTS[palette])
+    color = next(color_it)
+    plt.plot(scan_domain, scan_vals, color=color, marker='o',
              label=r'Likelihood Scan for $'+parameter_name+'$')
     #plot the lines indicating the differnt sections
-    xmin, xmax, ymin, ymax = plt.axis()
+    xmin, xmax, _ymin, ymax = plt.axis()
     #horizontal lines for the minimum and the min+1/2 lines
+    color = next(color_it)
     if not plot_delta:
-        plt.hlines(y=min_nll, xmin=xmin, xmax=xmax, color='blue', label='NLL minimum')
-    plt.hlines(y=min_nll+(1./2.), xmin=xmin, xmax=xmax, color='lightblue', linestyle='--',
+        plt.hlines(y=min_nll, xmin=xmin, xmax=xmax, color=color, label='NLL minimum')
+    color = next(color_it)
+    plt.hlines(y=min_nll+(1./2.), xmin=xmin, xmax=xmax, color=color, linestyle='--',
                label='standard deviation')
     #vertical lines for the center and the left and right standard deviations
-    plt.vlines(x=scan_domain[ls_idx], ymin=scan_vals[ls_idx], ymax=ymax, color='cyan',
+    color = next(color_it)
+    color = next(color_it)
+    plt.vlines(x=scan_domain[ls_idx], ymin=scan_vals[ls_idx], ymax=ymax, color=color,
                label=r'$\hat{'+parameter_name+'} \pm \sigma$')
-    plt.vlines(x=scan_domain[rs_idx], ymax=ymax, ymin=scan_vals[rs_idx], color='cyan')
-    plt.vlines(x=scan_domain[min_nll_idx], ymin=min_nll, ymax=ymax, color='gray',
+    plt.vlines(x=scan_domain[rs_idx], ymax=ymax, ymin=scan_vals[rs_idx], color=color)
+    plt.vlines(x=scan_domain[min_nll_idx], ymin=min_nll, ymax=ymax, color='slategray',
                label=r'$\hat{ '+parameter_name+'}_{ ML } = '+str(np.round(scan_domain[min_nll_idx],4))+'$')
     #grid and legend
     plt.grid(linestyle=':')
     plt.legend()
+    plt.title(title)
     plt.savefig('{}_likelihood_scan.svg'.format(parameter_name))
     plt.show()
 
-def plot_parameter_variation_over_errorbar(hist, bin_edges, model, fitted_params, scale_dependent_params, var_param_idx, var_vals, density=False):
-    colors = ['navy', 'blue', 'royalblue', 'dodgerblue', 'deepskyblue', 'darkturquoise']
+def plot_parameter_variation_over_errorbar(hist, bin_edges, model, fitted_params,
+                                           scale_dependent_params, var_param_idx, var_vals,
+                                           param_name=r'\theta', color='blue-green', density=False,
+                                           save=False, title='Variation of a parameter'):
+    """plots a sequence of functions where the parameter indicated by `var_param_idx` is run
+    through all values in `var_vals` while all other parameters stay unchanged (set to the
+    value in fitted_params. They are underlayed with data to which the function params are
+    supposed to be fitted. The scale is automatically adjusted to match the fit function and
+    the data in the histogram given by hist and bin edges. The indicies of the scale
+    dependent parameters are given in scale_dependent_parameters. The color of the curves can be
+    selected from a range of pallets ('blue-green', 'green-yellow', and 'red-yellow').
+    """
+    colors = PALETTS[color]
     #scale the scalable parameters to the scale of the histogramm
     #first determin the area unter the histogram as an estimate for area
     bin_width = bin_edges[1:] - bin_edges[:-1]
@@ -61,11 +87,7 @@ def plot_parameter_variation_over_errorbar(hist, bin_edges, model, fitted_params
     bounds = (min(bin_edges), max(bin_edges))
     model_int = stm.numerical_integrate_1D(model, bounds, *fitted_params)
     #the scale is defined as the ratio of the areas
-    print(fitted_params)
     scale = hist_int / model_int
-    print(hist_int)
-    print(model_int)
-    print(scale)
     #prepare variables for the plot
     domain = np.linspace(bounds[0], bounds[1], 1000)
     bin_centers = stm.bin_centers(bin_edges)
@@ -73,12 +95,21 @@ def plot_parameter_variation_over_errorbar(hist, bin_edges, model, fitted_params
     #do the parameter variation
     for var_val, color in zip(var_vals, cyclic_colors):
         #set the parameter to vary to the value of the current iteration
-        plot_params = [var_val if i == var_param_idx else param for i, param in enumerate(fitted_params)]
+        plot_params = [var_val if i == var_param_idx else param
+                       for i, param in enumerate(fitted_params)]
         #apply the scaling
-        plot_params = [scale*param if i in scale_dependent_params else param for i, param in enumerate(plot_params)]
+        plot_params = [scale*param if i in scale_dependent_params else param
+                       for i, param in enumerate(plot_params)]
         #plot
-        plt.plot(domain, model(domain, *plot_params), color=color)
-    #plot the
+        plt.plot(domain, model(domain, *plot_params), color=color,
+                 label=r'$'+param_name+r'$ = '+str(np.round(var_val, 3)))
+    #plot the errorbars
     yerr = np.sqrt(hist)
-    plt.errorbar(bin_centers, hist, yerr=yerr, color=next(cyclic_colors), linestyle='')
+    plt.errorbar(bin_centers, hist, yerr=yerr, color='black', marker='o',
+                 linestyle='', label='Events')
+    #draw the supplemental information
+    plt.grid(linestyle=':')
+    plt.title(r'Variation of $'+param_name+'$')
+    plt.legend()
+    plt.savefig('variation_of_param_'+param_name+'.svg')
     plt.show()
